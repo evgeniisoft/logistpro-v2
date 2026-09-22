6578b802055c1e02f55ca8b1f37750da746e9258
evgeniisoft <evgenijswg@gmail.com>
2026-09-22 15:48:05 +0300

    Create db.js

---
+++ api/_lib/db.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+let pool;
+
+function getPool() {
+    if (!pool) {
+        pool = new Pool({
+            connectionString: process.env.POSTGRES_URL,
+            ssl: { rejectUnauthorized: false }
+        });
+    }
+    return pool;
+}
+
+async function query(text, params) {
+    const client = await getPool().connect();
+    try {
+        const result = await client.query(text, params);
+        return result;
+    } finally {
+        client.release();
+    }
+}
+
+module.exports = { query };
